7819b638c50048d7150b8b6400f88b6271421360
SaraSorlin <sorlin82@gmail.com>
2023-10-18 14:18:43 +0200

    Dela upp klass istället för en
    Delade upp klasser i egna filer

---
+++ Band.js
@@ -0,0 +1,85 @@
+
+export class Band {
+
+    #name;
+    #info;
+    #yearstarted;
+    #yearended;
+    #current;
+    #earlier;
+  
+    constructor(name, yearstarted= '', yearended= '') {
+      this.#name = name;
+      this.#info = 'Band';
+      this.#yearstarted = yearstarted;
+      this.#yearended = yearended;
+      this.#current = [];
+      this.#earlier = [];
+    }
+  
+    get name() { 
+        return this.#name; 
+    }
+
+    set name(value) {
+        this.#name = value;
+    }
+
+
+    get information(){
+      return this.#info; 
+    }
+  
+    set information(newinfo) {
+      this.#info = newinfo;
+    }
+  
+    get yearstarted() {
+        return this.#yearstarted;
+        }
+
+    set yearstarted(newyearstart) {
+        this.#yearstarted = newyearstart;
+    }
+
+
+    get yearended() {
+      return this.#yearended;
+    }
+  
+     set yearended(newyear) {
+      this.#yearended = newyear;
+    }
+  
+    get current() {
+      return this.#current;
+  
+    }
+  
+    set current(newcurrent) {
+      this.#current.push(newcurrent);
+    }
+  
+    get earlier() {
+      return this.#earlier;
+    }
+  
+    set earlier(newearlier) {
+      this.#earlier.push(newearlier);
+    }
+  
+  
+    // Skapar ett objekt med bandets egenskaps information. 
+    // Används när vi ska skicka in till "databasen.json". 
+    databandInfo() {
+    return {
+        Name: this.#name,
+        Info: this.#info,
+        Yearstarted: this.#yearstarted,
+        Yearended: this.#yearended,
+        Current: this.#current,
+        Earlier: this.#earlier,
+     
+    };
+    }
+}
---
+++ Musiker.js
@@ -0,0 +1,79 @@
+
+export class Musiker {
+
+    #name;
+    #info;
+    #yearbirth;
+    #instrumentations;
+    #current;
+    #earlier;
+  
+    constructor(name, yearbirth= '') {
+      this.#name = name;
+      this.#info = 'Musiker';
+      this.#yearbirth = yearbirth;
+      this.#instrumentations = [];
+      this.#current = [];
+      this.#earlier = [];
+    }
+
+    get name() {
+    return this.#name;
+    }
+
+    set name(newname) {
+    this.#name = newname;
+    }
+
+    get info() {
+    return this.#info;
+    }
+
+
+    get Instrument() {
+      return this.#instrumentations;
+    }
+  
+    set Instrument(newinstrument) {
+      this.#instrumentations.push(newinstrument);
+    }
+  
+    get YearBirth() {
+      return this.#yearbirth;
+    }
+  
+    set YearBirth(newyearbirth) {
+      this.#yearbirth = newyearbirth;
+    }
+
+    set current(newcurrent) {
+        this.#current.push(newcurrent);
+    }
+    
+    get current() {
+        return this.#current;
+    }
+
+    set earlier(newearlier) {
+        this.#earlier.push(newearlier);
+    }
+  
+    get earlier() {
+        return this.#earlier;
+    }
+  
+      // Skapar ett objekt med musikerns egenskaps information. 
+    // Används när vi ska skicka in till "databasen.json". 
+
+    datamusikerInfo() {
+        return {
+          Name: this.#name,
+          Info: this.#info,
+          Yearbirth: this.#yearbirth,
+          Instrument: this.#instrumentations,
+          Current: this.#current,
+          Earlier: this.#earlier,
+        
+        };
+      }
+    }
